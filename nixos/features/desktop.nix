00f8cc08a47ce639ba97e37b43f9efd33d6fa58a
GroboChan <230193800+grobo-chan@users.noreply.github.com>
2026-05-21 12:55:53 +0530

    gparted

--- nixos/features/desktop.nix
+++ nixos/features/desktop.nix
@@ -1,6 +1,7 @@
 {self, ...}: {
   flake.nixosModules.desktop = {
     pkgs,
+    config,
     ...
   }: {
     imports = [
@@ -80,6 +81,16 @@
     environment.systemPackages = [
       pkgs.kdePackages.dolphin
       pkgs.ddcutil
-    ];
+        # run gparted with all the permissions crap fixed, I don't want it permanently installed
+        (pkgs.writeShellApplication {
+          name = "gparted";
+          # fix Authorization required, but no authorization protocol specified error
+          # fix gparted "cannot open display: :0" error
+          # respectively
+          text = /* sh */ ''
+            nix-shell -p xhost gparted ntfs3g --command "xhost si:localuser:root && xhost +local:${config.preferences.user.name} && sudo gparted"
+          '';
+        })
+      ];
   };
 }
--- nixos/hosts/europa/hardware-configuration.nix
+++ nixos/hosts/europa/hardware-configuration.nix
@@ -14,17 +14,7 @@
     boot.initrd.kernelModules = [];
     boot.kernelModules = ["kvm-intel"];
     boot.extraModulePackages = [];
-
-    boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_7_0.override {
-      argsOverride = rec {
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
-          sha256 = "sha256-u39tgLOHx1e30Uu5MCj8uQ95PFwNNnc27oFaEAs4kfA=";
-        };
-        version = "7.0";
-        modDirVersion = "7.0.0";
-      };
-    });
+    boot.kernelPackages = pkgs.linuxPackages_7_0;
 
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/57f5e02a-7418-4ba3-9f3f-3ee3537abd00";
