aba53c6fa042f546fe89d300f2294d8b3ba802c7
GroboChan <230193800+grobo-chan@users.noreply.github.com>
2026-06-20 12:46:59 +0530

    i forgor to persist the ssh folder oops

--- nixos/features/ssh-server.nix
+++ nixos/features/ssh-server.nix
@@ -12,5 +12,15 @@
     };
 
     services.fail2ban.enable = true;
+
+    persistance.sys.files = map (x: {
+      file = x;
+      mode = "0600";
+    }) [
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+    ];
   };
 }
